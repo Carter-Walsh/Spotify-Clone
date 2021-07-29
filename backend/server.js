f0ed46e4b4e44f06d905de77a45b12d095eb1753
Carter Walsh <Cwalsh616@gmail.com>
2021-07-29 15:04:47 -0500

    tmodifications to deploy

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ const express = require("express");
 const SpotifyWebApi = require("spotify-web-api-node");
 const cors = require("cors");
 const bodyparser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyparser.json());
 app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+});
 
 app.post("/login", (req, res) => {
     const code = req.body.code;
